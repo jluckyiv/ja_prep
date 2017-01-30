11574fd5dbc7854323e48ad108ccb5a95eb2a972
jluckyiv <jack.lucky.iv@gmail.com>
2017-01-29 23:05:13 -0800

    Count prelims

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+var extractHearingType = function(hearingRow) {
+  return hearingRow.querySelectorAll('td')[4].textContent.trim();
+};
+var isPrelim = function(hearingRow) {
+  return extractHearingType(hearingRow) == "PRELIMINARY HEARING";
+};
 var hearings = document.querySelectorAll('tr[id^="tr_row"]');
+var prelims = hearings.filter(isPrelim);
 console.log("There are " + hearings.length + " hearings.");
+console.log("There are " + prelims.length + " prelims.");
