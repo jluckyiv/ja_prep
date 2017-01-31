961176c12360b513bc61e93d8ca798d1747e47d2
Jack Lucky <jack.lucky.iv@gmail.com>
2017-01-31 09:45:54 -0800

    Add /dist/ to url for bundle.js

--- bookmarklets/bookmarklet.js
+++ bookmarklets/bookmarklet.js
@@ -1,4 +1,4 @@
 document.getElementsByTagName('body')[0]
   .appendChild(document.createElement('script'))
-  .src='https://rawgit.com/jluckyiv/ja_prep/master/bundle.js?'+Math.random();
+  .src='https://rawgit.com/jluckyiv/ja_prep/master/dist/bundle.js?'+Math.random();
 
