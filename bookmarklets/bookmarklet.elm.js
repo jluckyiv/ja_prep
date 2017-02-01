d551293379b6bdf79b72556cbaedfdcd99c32f3c
Jack Lucky <jack.lucky.iv@gmail.com>
2017-02-01 11:00:59 -0800

    Remove old script nodes and replace

--- bookmarklets/bookmarklet.elm.js
+++ bookmarklets/bookmarklet.elm.js
@@ -1,12 +1,24 @@
-var elmDiv = document.createElement('div');
-elmDiv.id='elm';
+var url =  'https://rawgit.com/jluckyiv/ja_prep/master/elm.js?'
+var source = url + Math.random();
+var divId = 'elm';
+var scriptId = 'elm-script';
+var body = document.getElementsByTagName('body')[0];
 
-var elmJs = document.createElement('script');
-elmJs.src='https://rawgit.com/jluckyiv/ja_prep/master/elm.js?'+Math.random();
-elmJs.onload=function() {
-  var node = document.getElementById('elm');
+// Remove old script and elm div
+var oldScript = document.getElementById(scriptId);
+if (oldScript) { body.removeChild(oldScript); }
+var oldDiv = document.getElementById(divId)
+if (oldDiv) { body.removeChild(oldDiv); }
+
+// Add new script and elm div
+var div = document.createElement('div');
+div.id = divId;
+var script = document.createElement('script');
+script.id = scriptId;
+script.src = source;
+script.onload=function() {
+  var node = document.getElementById(divId);
   Elm.App.embed(node);
 };
-
-document.body.appendChild(elmDiv);
-document.body.appendChild(elmJs);
+body.appendChild(div);
+body.appendChild(script);
--- bookmarklets/bookmarklet.js
+++ bookmarklets/bookmarklet.js
@@ -1,4 +1,14 @@
-document.getElementsByTagName('body')[0]
-  .appendChild(document.createElement('script'))
-  .src='https://rawgit.com/jluckyiv/ja_prep/master/dist/bundle.js?'+Math.random();
+var url = 'https://rawgit.com/jluckyiv/ja_prep/master/dist/bundle.js?';
+var source = url + Math.random();
+var id = 'ja_prep';
+var body = document.getElementsByTagName('body')[0];
 
+// Remove old script
+var oldScript = document.getElementById(id);
+if (oldScript) { body.removeChild(oldScript); }
+
+// Append new script
+var script = document.createElement('script');
+script.id = 'ja_prep';
+script.src = source;
+body.appendChild(script);
