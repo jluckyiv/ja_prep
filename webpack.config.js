0fccfb9f254b5da243a78657c7bbe51a71019c4b
Jack Lucky <jack.lucky.iv@gmail.com>
2017-01-31 22:01:12 -0800

    Add jshint

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+
+      }
+    ],
   }
 };
