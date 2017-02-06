343eb6079e4f77330375f7c999fcf3bdae4603db
Jack Lucky <jack.lucky.iv@gmail.com>
2017-02-05 22:30:20 -0800

    Parse case reports

---
+++ app/casereport.js
@@ -0,0 +1,116 @@
+/*jshint esversion: 6 */
+
+import { NodeHelpers } from './node-helpers';
+
+const ACTION_NODE_SELECTOR = `tr > input[id^="hidKey"]`;
+const PROBATION_DIV_ID = `divProbationBody`;
+const PROBATION_TERMS_SELECTOR = `#${PROBATION_DIV_ID} > #table-box3 > tbody > tr`;
+const PROBATION_SUMMARY_SELECTOR = `#${PROBATION_DIV_ID} > #table-title-left td`;
+
+const parseProbationInfo = node => {
+  let result = {};
+  const nodes = node.querySelectorAll(PROBATION_SUMMARY_SELECTOR);
+  if (nodes && nodes.length) {
+    result.type = nodes[0].textContent.trim();
+    result.granted = nodes[1].textContent.trim();
+    result.expires = nodes[2].textContent.trim();
+  }
+  return result;
+};
+
+const parseProbationTerms = node => {
+  let result = [];
+  const nodes = node.querySelectorAll(PROBATION_TERMS_SELECTOR);
+  if (nodes && nodes.length) {
+    for (let i = 1, l = nodes.length; i < l; i += 1) {
+      const number = parseInt(nodes[i].children[1].textContent.trim());
+      const term = nodes[i].children[3].textContent.trim();
+      result.push({number: number, term: term});
+    }
+  }
+  return result;
+};
+
+const parseAction = node => {
+  let result = {};
+  const parent = node.parentNode;
+  const cells = parent.querySelectorAll('td');
+
+  result.code = parseActionCode(node.value).trim();
+  result.date = cells[0].textContent.trim().substr(0,10);
+  result.description = cells[1].textContent.trim();
+  const disposition = cells[2].textContent.trim();
+  if (disposition) { result.disposition = disposition; }
+  const actionType = cells[3].textContent.trim();
+  if (actionType) { result.actionType = actionType; }
+  const imageLink = cells[5].querySelector('a');
+  if (imageLink) { result.imageUrl = imageLink.href.replace('..', 'http://riv-ja1/JA'); }
+  return result;
+};
+
+const parseActions = html => {
+  let result = [];
+  const nodes = html.querySelectorAll(ACTION_NODE_SELECTOR);
+  if (nodes && nodes.length) {
+    result = result.concat(NodeHelpers.toArray(nodes, parseAction));
+  }
+  return result;
+};
+
+const parseActionCode = value => {
+  const segments = value.split(',');
+  const result = segments[9];
+  return result;
+};
+
+const hasDisclosure = html => {
+  const text = html.body.textContent;
+  const result = text.includes("DISCLOSURE FILED") || 
+    text.includes("COURT DISCLOSES THAT JUDGE LUCKY'S WIFE");
+  return result;
+};
+const needsDisclosure = caseReport => caseReport.needsDisclosure;
+
+const isProof = action => {
+  const result = action.description.includes('PROOF OF');
+  return result;
+};
+
+const isTermination = action => {
+  const result = action.description.includes('PROGRAM TERMINATION');
+  return result;
+};
+
+const isDeadline = probationTerm => {
+  const result = probationTerm.term.search(/(BY|BEFORE) \d\d\/\d\d\/\d\d\d\d/) > -1;
+  return result;
+};
+
+const create = (html, action) => {
+  let result = {};
+  result.needsDisclosure = !hasDisclosure(html);
+  result.actions = parseActions(html);
+  result.probationTerms = parseProbationTerms(html); 
+  result.probationInfo = parseProbationInfo(html);
+  return result;
+};
+
+const actions = caseReport => caseReport.actions;
+const deadlines = caseReport => caseReport.probationTerms.filter(isDeadline);
+const probationInfo = caseReport => caseReport.probationInfo;
+const probationTerms = caseReport => caseReport.probationTerms;
+const proofs = caseReport => caseReport.actions.filter(isProof); 
+const terminations = caseReport => caseReport.actions.filter(isTermination);
+
+let CaseReport = {};
+CaseReport.create = create;
+CaseReport.actions = actions;
+CaseReport.needsDisclosure = needsDisclosure;
+CaseReport.parseActionCode = parseActionCode;
+CaseReport.probationInfo = probationInfo;
+CaseReport.probationTerms = probationTerms;
+CaseReport.terminations = terminations;
+CaseReport.proofs = proofs;
+CaseReport.deadlines = deadlines;
+
+export { CaseReport };
---
+++ test/casereport-test.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const fs = require('fs');
+const jsdom = require('jsdom');
+import { expect } from 'chai';
+import { CaseReport } from '../app/casereport'
+
+describe('CaseReport', () => {
+
+  describe('.needsDisclosure', () => {
+    const blankDocument = () => {
+      return jsdom.jsdom('<!doctype html><html><body>' +
+        '<div id="test-div"></div></body></html>')
+    };
+
+    let doc, testDiv;;
+    beforeEach(() => {
+      doc = blankDocument();
+      testDiv = doc.getElementById('test-div');
+    });
+
+    it("should return false for COURT DISCLOSES THAT JUDGE LUCKY'S WIFE", () => {
+      const doc = jsdom.jsdom('<!doctype html><html><body>' +
+        "<div>COURT DISCLOSES THAT JUDGE LUCKY'S WIFE</div>" +
+        '</body></html>')
+      const subject = CaseReport.create(doc);
+      expect(CaseReport.needsDisclosure(subject)).to.be.false;
+    });
+
+    it('should return false for DISCLOSURE FILED', () => {
+      testDiv.textContent = "DISCLOSURE FILED";
+      const subject = CaseReport.create(doc);
+      expect(CaseReport.needsDisclosure(subject)).to.be.false;
+    });
+
+    it('should return true otherwise', () => {
+      testDiv.textContent = "something other than disclosure langauge";
+      const subject = CaseReport.create(doc);
+      expect(CaseReport.needsDisclosure(subject)).to.be.true;
+    });
+  });
+
+  describe('.parseActionCode()', () => {
+    it("should return the action code for the action", () => {
+      const value = "OTS,0,C,RIF1501913,,2300428,20160310,13.3,,FICA,W091208"
+      const subject = CaseReport.parseActionCode(value);
+      expect(subject).to.equal('FICA');
+    });
+  });
+
+  describe('.create()', () => {
+    describe('.actions', () => {
+      const loadRIF1501913 = () => {
+        const html = fs.readFileSync('./test/fixtures/RIF1501913 - Case Report.htm');
+        const doc = jsdom.jsdom(html);
+        return doc;
+      };
+
+      let doc, caseReport;;
+      before(() => {
+        doc = loadRIF1501913();
+        caseReport = CaseReport.create(doc);
+      });
+
+      it("should return an array of action", () => {
+        const subject = CaseReport.actions(caseReport);
+        expect(subject.length).to.equal(94);
+      });
+
+      it("should parse a document action", () => {
+        const subject = CaseReport.actions(caseReport)[11];
+        expect(subject.code).to.equal('RCAO');
+        expect(subject.date).to.equal('09/30/2016');
+        expect(subject.description).to
+          .equal('REQUEST FOR CALENDAR ADD-ON FILED.');
+        expect(subject.imageUrl).to
+          .include('http://riv-ja1/JA/ImageLinkWindow');
+        expect(subject.imageUrl).to.include('ImgKey=1314444815');
+      });
+
+      it("should parse a hearing action", () => {
+        const subject = CaseReport.actions(caseReport)[1];
+        expect(subject.code).to.equal('HDVP');
+        expect(subject.date).to.equal('02/06/2017');
+        expect(subject.description).to
+          .equal('HEARING RE: DOMESTIC VIOLENCE PROGRESS.');
+        expect(subject.disposition).to.equal('ACTIVE');
+      });
+
+      it("should parse probation information", () => {
+        const subject = CaseReport.probationInfo(caseReport);
+        expect(subject.type).to.equal('SUMMARY');
+        expect(subject.granted).to.equal('07/07/2016');
+        expect(subject.expires).to.equal('07/07/2019');
+      });
+
+      it("should parse probation terms", () => {
+        const subject = CaseReport.probationTerms(caseReport);
+        expect(subject[0].term).to
+          .equal('OBEY ALL LAWS, ORDINANCES, AND COURT ORDERS.');
+        expect(subject[2].term).to
+          .equal('149 DAYS TO BE SERVED IN THE WORK RELEASE PROGRAM. ' +
+            'REPORT ON OR BEFORE 07/27/2016.');
+      });
+
+      it("should find program terminations", () => {
+        const subject = CaseReport.terminations(caseReport);
+        expect(subject[0].description).to.include('NOT FORWARDED');
+        expect(subject[1].description).to.include('WORK RELEASE');
+      });
+
+      it("should find proof of enrollment/completion", () => {
+        const subject = CaseReport.proofs(caseReport);
+        expect(subject[0].description).to.include('COMPLETION ELECTRONIC MONITORING');
+        expect(subject[1].description).to.include('ENROLLMENT ELECTRONIC MONITORING');
+        expect(subject[2].description).to.include('ENROLLMENT - DOMESTIC VIOLENCE');
+        expect(subject[3].description).to.include('ENROLLMENT - DOMESTIC VIOLENCE');
+      });
+
+      it("should find deadlines", () => {
+        const subject = CaseReport.deadlines(caseReport);
+        expect(subject.length).to.equal(4);
+      });
+    });
+  });
+});
