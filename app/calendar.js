45fb832cf809dba6fe32c8b8c34ec47435eaae13
Jack Lucky <jack.lucky.iv@gmail.com>
2017-02-05 22:30:08 -0800

    Parse calendars

--- app/calendar.js
+++ app/calendar.js
@@ -1,60 +1,76 @@
 /*jshint esversion: 6 */
 
+import { Ajax } from './ajax';
+import { CaseReport } from './casereport';
 import { NodeHelpers} from './node-helpers';
 import { Urls } from './urls';
-import { Ajax } from './ajax';
 
 const HEARING_NODE_SELECTOR = 'tr[id^="tr_row"]';
+const hearingNodes = node  => document.querySelectorAll(HEARING_NODE_SELECTOR);
+const hearingRow = hearing => document.getElementById(hearing.nodeId);
+const infoRow = hearing => document.getElementById(hearing.infoNodeId);
 
-const findOrCreateInfoRow = hearing => {
-  let row, buttonCell, button, infoCell;
-  const hearingNode = document.getElementById(hearing.nodeId);
-  const nodeId = hearing.infoNodeId;
-  row = document.getElementById(nodeId);
-  if (!row) {
-    row = document.createElement('tr');
-    row.id = nodeId;
-    buttonCell = document.createElement('td');
-    button = document.createElement('button');
-    button.textContent = 'Info';
-    button.addEventListener('click', function(){loadInfo(hearing);}, false);
-    buttonCell.appendChild(button);
-    row.appendChild(buttonCell);
-    infoCell = document.createElement('td');
-    infoCell.colSpan = '6';
-    row.appendChild(infoCell);
-    NodeHelpers.insertAfter(row, hearingNode);
-  }
-  row.style.display = hearingNode.style.display;
+const infoButton = hearing => infoRow(hearing).children[0].children[0];
+const infoCell = hearing => infoRow(hearing).children[1];
+
+const setButtonText = (text, hearing) => infoButton(hearing).textContent = text; 
+const disableButton = (hearing) => infoButton(hearing).disabled = true;
+const enableButton = (hearing) => infoButton(hearing).disabled = false;
+const setInfoText = (text, hearing) => infoCell(hearing).textContent = text;
+
+const deleteInfoRow = hearing => NodeHelpers.removeNode(infoRow(hearing));
+const createInfoRow = hearing => {
+  let row = document.createElement('tr');
+  row.id = hearing.infoNodeId;
+  row.setAttribute('class', 'hearing-info');
   return row;
 };
 
-const setInfoText = (text, hearing) => {
-  const node = findOrCreateInfoRow(hearing);
-  node.children[1].textContent = text;
+const createInfoButtonCell = hearing => {
+  let buttonCell = document.createElement('td');
+  buttonCell.setAttribute('class', 'info-button-cell');
+  return buttonCell;
 };
 
-const addInfoRows = (hearings, callback) => {
-  hearings.forEach( hearing => {
-    let row = findOrCreateInfoRow(hearing);
+const createInfoButton = hearing => {
+  let button = document.createElement('button');
+  button.textContent = 'Info';
+  button.setAttribute('class', 'info-button');
+  button.addEventListener('click', () => getInfo(hearing), false);
+  return button;
+};
+
+const createInfoCell = hearing => {
+  let infoCell = document.createElement('td');
+  infoCell.setAttribute('class', 'info-cell');
+  infoCell.colSpan = '6';
+  return infoCell;
+};
+
+const appendInfoRow = hearing => {
+  deleteInfoRow(hearing);
+
+  const hearingNode = hearingRow(hearing);
+  let row = createInfoRow(hearing);
+  let buttonCell = createInfoButtonCell(hearing);
+  let button = createInfoButton(hearing);
+  let infoCell = createInfoCell(hearing);
+
+  buttonCell.appendChild(button);
+  row.appendChild(buttonCell);
+  row.appendChild(infoCell);
+  row.style.display = hearingNode.style.display;
+  NodeHelpers.insertAfter(row, hearingNode);
+};
+
+const createInfoRows = (hearings, callback) => {
+  hearings.forEach(hearing => {
+    appendInfoRow(hearing);
     if (callback) {
-      callback(row, hearing);
+      callback(hearing);
     }
   });
 };
-
-const loadInfo = hearing => {
-  setInfoText('Loading info for ' + hearing.casenumber, hearing);
-  Ajax.get(hearing.urls.info, function() {
-    setInfoText('Info loaded for ' + hearing.casenumber, hearing);
-  });
-};
-
-const getInfo = hearings => {
-  addInfoRows(hearings);
-};
-
-const hearingNodes = node  => document.querySelectorAll(HEARING_NODE_SELECTOR);
 
 const parseHearing = node => {
   let hearing = {};
@@ -76,8 +92,39 @@ const parseHearings = node => {
   return NodeHelpers.toArray(hearingNodes(node), parseHearing);
 };
 
+const updateInfoText = (hearing) => {
+  const info = hearing.info;
+  if (info) {
+    setInfoText(JSON.stringify(info), hearing);
+  }
+};
+
+const setCachedValue = hearing => {
+  const key = hearing.casenumber + hearing.defnbr;
+  localStorage.setItem(key, JSON.stringify(hearing.info));
+};
+
+const getCachedValue = hearing => {
+  const key = hearing.casenumber + hearing.defnbr;
+  hearing.info = JSON.parse(localStorage.getItem(key));
+};
+
+const getInfo = function(hearing) {
+  setButtonText('Updating', hearing);
+  setInfoText('Loading info for ' + hearing.casenumber, hearing);
+  disableButton(hearing);
+  getCachedValue(hearing);
+  updateInfoText(hearing);
+  Ajax.get(hearing.urls.report, function() {
+    hearing.info = CaseReport.create(this.responseXML, hearing);
+    setCachedValue(hearing);
+    updateInfoText(hearing);
+    setButtonText('Loaded', hearing);
+  });
+};
+
 let Calendar = {};
-Calendar.getInfo = getInfo;
+Calendar.createInfoRows = createInfoRows;
 Calendar.parseHearings = parseHearings;
 
 export { Calendar };
---
+++ test/calendar-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const jsdom = require('jsdom');
+import { expect } from 'chai';
+import { Calendar } from '../app/calendar'
+
+describe ('Calendar', () => {
+  describe('.parseHearings()', () => {
+
+    let calendar;
+    before(function() {
+      const html = fs.readFileSync('./test/fixtures/01_30_2017_D33.htm');
+      global.document = jsdom.jsdom(html);
+      calendar = Calendar.parseHearings(document.body);
+    });
+
+    it("should return a list of hearings", () => {
+      const subject = calendar;
+      expect(subject.length).to.equal(14);
+    });
+
+    it("should correctly parse a single hearing", () => {
+      const subject = calendar[0];
+      expect(subject.nodeId).to.equal('tr_row0');
+      expect(subject.infoNodeId).to.equal('info_tr_row0');
+      expect(subject.heard).to.be.false;
+      expect(subject.time).to.equal('8:30 AM');
+      expect(subject.casenumber).to.equal('RIM1411527');
+      expect(subject.name).to.equal('AGUILAR GONZALEZ, ANTHONY JUNIOR');
+      expect(subject.defnbr).to.equal('2671022');
+      expect(subject.description).to.equal('PROBATION HEARING RE: DV & CS');
+      expect(subject.charges).to.include('243(E)(1)');
+      expect(subject.urls).to.have.all.keys([ 'actions', 'charges', 'dmv',
+        'fine', 'images', 'info', 'minutes', 'notes', 'perm', 'probation',
+        'report', 'status' ]);
+    });
+  });
+
+
+});
