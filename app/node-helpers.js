b11e1a77e62f19ad58bde7750ab8bf5596a2b678
Jack Lucky <jack.lucky.iv@gmail.com>
2017-02-05 22:28:26 -0800

    Add removeNode function

--- app/node-helpers.js
+++ app/node-helpers.js
@@ -6,6 +6,10 @@ const insertAfter = (newNode, referenceNode) => {
   parent.insertBefore(newNode, next);
 };
 
+const removeNode = node => {
+  if (node) { node.parentNode.removeChild(node); }
+};
+
 const toArray = (nodeList, callback) => {
   let a = [];
   for (let i = 0, l = nodeList.length; i < l; i += 1) {
@@ -20,6 +24,7 @@ const toArray = (nodeList, callback) => {
 
 let NodeHelpers = {};
 NodeHelpers.insertAfter = insertAfter;
+NodeHelpers.removeNode = removeNode;
 NodeHelpers.toArray = toArray;
 
 export { NodeHelpers };
